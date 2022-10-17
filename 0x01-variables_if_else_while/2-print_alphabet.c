16cbef86c296fa6bb95ffb7380e3fd4d3e4bb8f7
westbliss <nikky_bliss@yahoo.com>
2022-10-17 06:24:31 -0700

    2 print

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio>
+
+/**
+ * main - prints the alphabet in lower case
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
